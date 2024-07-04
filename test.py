3bb5c361f3b58b37ce3f7162e9bc705a00e1c593
kezhengjie <kezhengjie@minigame.vip>
2024-07-04 18:15:49 +0800

    update

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import os
 
 
-data = "\\a\\b\\c\\d"
-data.replace("\\", "/")
-print(data)
-l = data.split("/")
-print(os.path.join(*l))
+for i in range(0, 10):
+    with open(f"base_library/{i}.txt", "w") as fd:
+        for j in range(0, i % 5):
+            fd.write(str(i + j))
